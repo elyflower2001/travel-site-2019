4302e5a3d6840cbaa0c58588ed12109623c64dab
Elisa <citterio.elisa@gmail.com>
2020-01-07 21:52:30 +0100

    video 37: header style

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ const postCSSPlugins = [
     require('postcss-mixins'),
     require('postcss-simple-vars'),
     require('postcss-nested'),
+    require('postcss-hexrgba'),
     require('autoprefixer')
 ];
  
