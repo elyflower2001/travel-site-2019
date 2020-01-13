0bda76b942aa5fdc50885d22bd1d8f2359360993
Elisa <citterio.elisa@gmail.com>
2020-01-13 10:36:38 +0100

    Added js and style to open the menu

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,9 @@
 import '../styles/styles.css';
+import MobileMenu from './modules/MobileMenu';
 
 if(module.hot) {
     module.hot.accept();
 }
 
+/* Mobile menu */
+let mobileMenu = new MobileMenu();
