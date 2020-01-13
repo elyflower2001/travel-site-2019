8dd3a7f1a8fa0f495e234b7f8d7ff19178762390
Elisa <citterio.elisa@gmail.com>
2020-01-13 22:52:23 +0100

    video 46: lodash e funzioni per migliorare scroll

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,31 +1,47 @@
+import throttle from 'lodash/throttle';
+
 class RevealOnScroll {
     constructor() {
         this.itemsToReveal = document.querySelectorAll('.feature-item');
         this.hideInitially();
+        this.scrollThrottle = throttle(this.calcCaller, 200).bind(this);
         this.events();
     }
 
     events() {
-        window.addEventListener('scroll', () => {
-            console.log('scroll function run');
-            this.itemsToReveal.forEach(el => {
-                this.calculateIfScrolledTo(el);
-            });
-        });
+        window.addEventListener('scroll', this.scrollThrottle);
     }
 
     hideInitially() {
-        this.itemsToReveal.forEach(function(el) {
-            el.classList.add('reveal-item')
+        this.itemsToReveal.forEach( el => {
+            el.classList.add('reveal-item');
+            el.isRevealed = false;
         });
+        this.itemsToReveal[this.itemsToReveal.length - 1].isLastItem = true;
     }
 
     calculateIfScrolledTo(el) {
         //console.log(el.getBoundingClientRect().y);
         let scrollPercent = (el.getBoundingClientRect().y / window.innerHeight) *100;
         if( scrollPercent < 75) {
+            console.log('element was calculated');
             el.classList.add('reveal-item--is-visible');
+            el.isRevealed = true;
+            if(el.isLastItem == true) {
+               console.log('is last item');
+               window.removeEventListener('scroll', this.scrollThrottle);
+            }
         }
+    }
+
+    calcCaller() {
+        console.log('scroll run');
+        this.itemsToReveal.forEach(el => {
+            if(el.isRevealed == false) {
+                console.log('isRevealed = false');
+                this.calculateIfScrolledTo(el);
+            }
+        });
     }
 }
 
