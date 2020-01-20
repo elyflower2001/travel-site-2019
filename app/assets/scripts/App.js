c0153bceb354ef3560e549f622d91820a2b0d456
Elisa <citterio.elisa@gmail.com>
2020-01-20 09:06:36 +0100

    video 48: rendere menu sticky

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,6 +1,7 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StyckyHeader';
 
 if(module.hot) {
     module.hot.accept();
@@ -8,6 +9,7 @@ if(module.hot) {
 
 /* Mobile menu */
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 //let revealOnScroll = new RevealOnScroll();
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
