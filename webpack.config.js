3d27f49f2563675b0bd8893a2e3005ec37a6d69c
Elisa <citterio.elisa@gmail.com>
2020-01-22 10:07:02 +0100

    video 55: config build process for go live

--- webpack.config.js
+++ webpack.config.js
@@ -1,29 +1,4 @@
-/*const path = require('path');
-
-const postCSSPlugins = [
-  require('postcss-simple-vars'),
-  require('postcss-nested'),
-  require('autoprefixer')
-];
-
-module.exports = {
-  entry: './app/assets/scripts/App.js',
-  output: {
-    filename: 'bundled.js',
-    path: path.resolve(__dirname, 'app')
-  },
-  mode: 'development',
-  watch: true,
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
-      }
-    ]
-  }
-}*/
-
+const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 
 const postCSSPlugins = [
@@ -34,30 +9,42 @@ const postCSSPlugins = [
     require('postcss-hexrgba'),
     require('autoprefixer')
 ];
- 
-module.exports = {
-    entry: './app/assets/scripts/App.js',
-    output: {
-        filename: 'bundled.js',
-        path: path.resolve(__dirname, 'app')
-    },
-    devServer: {
-        before: function(app, server) {
-            server._watch('./app/**/*.html')
-        },
-        contentBase: path.join(__dirname, 'app'),
-        hot: true,
-        port: 8081,
-        host: '0.0.0.0'
-    },
-    mode: 'development',
-    //watch: true,
-    module: {
-        rules: [
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
-            }
-        ]
-    }
+
+let config = {
+  entry: './app/assets/scripts/App.js',
+  module: {
+    rules: [
+        {
+            test: /\.css$/i,
+            use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+        }
+    ]
+  }
 }
+
+if(currentTask == 'dev') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'app')
+  },
+  config.devServer = {
+    before: function(app, server) {
+        server._watch('./app/**/*.html')
+    },
+    contentBase: path.join(__dirname, 'app'),
+    hot: true,
+    port: 8081,
+    host: '0.0.0.0'
+  },
+  config.mode = 'development'
+}
+
+if(currentTask == 'build') {
+  config.output = {
+    filename: 'bundled.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  config.mode = 'production'
+}
+
+module.exports = config;
