b1b3a636e6e8c9e4039425ab8b5684825eb4c633
Elisa <citterio.elisa@gmail.com>
2020-01-22 17:16:33 +0100

    video 56: processo du build - js con chunkhash e file css

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 const postCSSPlugins = [
     require('postcss-import'),
@@ -10,19 +12,22 @@ const postCSSPlugins = [
     require('autoprefixer')
 ];
 
+let cssConfig =  {
+  test: /\.css$/i,
+  use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
+}
+
 let config = {
   entry: './app/assets/scripts/App.js',
   module: {
     rules: [
-        {
-            test: /\.css$/i,
-            use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
-        }
+       cssConfig
     ]
   }
 }
 
 if(currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader'),
   config.output = {
     filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
@@ -40,11 +45,18 @@ if(currentTask == 'dev') {
 }
 
 if(currentTask == 'build') {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader),
+  postCSSPlugins.push(require('cssnano')),
   config.output = {
-    filename: 'bundled.js',
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
     path: path.resolve(__dirname, 'dist')
   },
-  config.mode = 'production'
+  config.mode = 'production',
+  config.optimization = {
+    splitChunks: {chunks: 'all'}
+  },
+  config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({filename: 'style.[chunkhash].css'})]
 }
 
 module.exports = config;
