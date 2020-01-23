7adfd6997d2bc0c344bbc6e26818353ff7ae768b
Elisa <citterio.elisa@gmail.com>
2020-01-23 17:11:34 +0100

    video 57-58: build per html e img + babel

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ const currentTask = process.env.npm_lifecycle_event;
 const path = require('path');
 const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const fse = require('fs-extra');
 
 const postCSSPlugins = [
     require('postcss-import'),
@@ -12,13 +14,31 @@ const postCSSPlugins = [
     require('autoprefixer')
 ];
 
+class RunAfterCompile {
+  apply(compiler) {
+    compiler.hooks.done.tap('copy images', function() {
+      fse.copySync('./app/assets/images','./dist/assets/images');
+    });
+  }
+}
+
 let cssConfig =  {
   test: /\.css$/i,
   use: ['css-loader?url=false', {loader: 'postcss-loader', options: {plugins: postCSSPlugins}}]
 }
 
+let pages = fse.readdirSync('./app').filter(function(file) {
+  return file.endsWith('.html');
+}).map(function(page) {
+  return new HtmlWebpackPlugin({
+    filename: page,
+    template: `./app/${page}`
+    });
+});
+
 let config = {
   entry: './app/assets/scripts/App.js',
+  plugins: pages,
   module: {
     rules: [
        cssConfig
@@ -45,6 +65,16 @@ if(currentTask == 'dev') {
 }
 
 if(currentTask == 'build') {
+  config.module.rules.push({
+    test:/\.js$/,
+    exclude: /(node_modules)/,
+    use: {
+      loader: 'babel-loader',
+      options: {
+        presets: ['@babel/preset-env']
+      }
+    }
+  });
   cssConfig.use.unshift(MiniCssExtractPlugin.loader),
   postCSSPlugins.push(require('cssnano')),
   config.output = {
@@ -56,7 +86,11 @@ if(currentTask == 'build') {
   config.optimization = {
     splitChunks: {chunks: 'all'}
   },
-  config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({filename: 'style.[chunkhash].css'})]
+  config.plugins.push(
+    new CleanWebpackPlugin(), 
+    new MiniCssExtractPlugin({filename: 'style.[chunkhash].css'}),
+    new RunAfterCompile()
+  )
 }
 
 module.exports = config;
